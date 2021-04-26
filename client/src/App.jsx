ee3b42bbbb097ad986937885765bc01dc289a864
Santiago Rincon <santiagorincon2001@gmail.com>
2021-04-26 10:08:18 -0500

    Using emulator firestore local

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,24 @@
 import Home from "./pages/Home";
 import "./App.scss"
-import { FooterView } from "./pages/Home/components/Footer/FooterView";
+import { FooterView} from "./pages/Home/components/Footer/FooterView";
+import { preloadDatabase, preloadFirestore, preloadRemoteConfig, preloadStorage, useFirebaseApp } from "reactfire";
+
 export default function App() {
+  const firebaseApp = useFirebaseApp();
+  console.log(process.env.REACT_APP_DBLOCAL)
+  if(process.env.REACT_APP_DBLOCAL === true){
+    const preloadSDKs = firebaseApp => {
+      return Promise.all([
+        preloadFirestore({
+          firebaseApp,
+          setup: firestore => {
+          return firestore().settings({host: 'localhost:8080', ssl: false});
+        }
+        }),
+      ]);
+    };
+    preloadSDKs(firebaseApp).then(() => Promise.resolve());
+  }
   return <div>
       <Home/>
       <FooterView/>
